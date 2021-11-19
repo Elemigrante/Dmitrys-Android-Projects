b4706e044db79efa49f881ba88a1a04976af4813
Neladnov Dmitry <gasdom@yandex.ru>
2021-11-19 18:14:52 +0300

    Initial commit

---
+++ app/src/main/java/com/dmitryneladnov/dmitrysandroidprojects/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dmitryneladnov.dmitrysandroidprojects;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
