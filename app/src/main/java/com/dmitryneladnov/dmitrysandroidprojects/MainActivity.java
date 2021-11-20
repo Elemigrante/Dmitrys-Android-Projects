b6a8e0bed2cb4d3bc780aaa4052bd0d68dbfd99e
Neladnov Dmitry <gasdom@yandex.ru>
2021-11-20 12:56:34 +0300

    feat: adding the Drawables

--- app/src/main/java/com/dmitryneladnov/dmitrysandroidprojects/MainActivity.java
+++ app/src/main/java/com/dmitryneladnov/dmitrysandroidprojects/MainActivity.java
@@ -14,26 +14,26 @@ public class MainActivity extends AppCompatActivity {
         Project[] projects = {
                 new Project("Getting Started App",
                         "First project, the default 'Hello World' app!",
-                        5
+                        R.drawable.getting_started
                 ),
                 new Project("Moti Quote App",
                         "Shows simple motivation quote",
-                        5
+                        R.drawable.quote
                 ),
                 new Project("BMI Calc",
                         "Calculate BMI and gives recommendations, checks age if someone is under 18. " +
                                 "By making this app I learned about Variables, Methods and Conditional Logic",
-                        5
+                        R.drawable.calculator
                 ),
                 new Project("Unit Converter",
                         "A basic converter to convert inches to meters (made by my own)," +
                                 " learning about Strings, Integers, Formatting, Parse int strings in to integers.",
-                        5
+                        R.drawable.tape
                 ),
                 new Project("The Dev Buffet",
                         "A menu app for a fictional restaurant," +
                                 " learning about Activities, Classes & Objects, Arrays, Intents and ListView.",
-                        5
+                        R.drawable.dev_buffet
                 ),
         };
     }
